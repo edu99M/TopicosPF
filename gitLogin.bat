fa770376c1930fa347f245dbcadd865f07569bbf
edu9M <edu99mendes@gmail.com>
2022-03-04 20:50:13 -0300

    58

---
+++ gitLogin.bat
@@ -0,0 +1,5 @@
+git config --global user.name "edu9M"
+git config --global user.email "edu99mendes@gmail.com"
+git init --initial-branch=main
+git remote add origin https://github.com/edu99M/TopicosPF.git
+timeout 5

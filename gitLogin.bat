bf3ad94add1d299105e12b5fce1906ea43ef48f4
edu9M <edu99mendes@gmail.com>
2022-03-04 20:51:12 -0300

    update timeout

--- gitLogin.bat
+++ gitLogin.bat
@@ -2,4 +2,4 @@ git config --global user.name "edu9M"
 git config --global user.email "edu99mendes@gmail.com"
 git init --initial-branch=main
 git remote add origin https://github.com/edu99M/TopicosPF.git
-timeout 5
+timeout 4
